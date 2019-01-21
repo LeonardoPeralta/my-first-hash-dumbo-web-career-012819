a7a693a2d0349290860df37ae780a87dece0db32
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-21 05:47:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,9 +16,9 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "whale bone corsets" => 2,
+                  "porcelain vases" => 3,
+                  "oil paintings" => 5
                  }
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 return shipping_manifest["oil paintings"]
